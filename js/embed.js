3dc642d00520e668df74d1838127e115072012b7
本間圭一 <keiichi-honma@813.co.jp>
2012-11-01 13:00:08 +0900

    gitkeep

---
+++ js/embed.js
@@ -0,0 +1,5 @@
+window.jQuery || document.write("<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js'></script>");
+if (navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('iPad') > 0 || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0) {
+} else {
+document.write("<link rel='stylesheet' href='http://simulator.813.co.jp/css/viewer.css' type='text/css' media='all' /><script type='text/javascript' src='http://simulator.813.co.jp/js/viewer.js'></script><div id='viewer'><iframe src='http://simulator.813.co.jp/iframe' style='z-index: 1;padding:0;margin:0;' scrolling='no' frameborder='0' width='240' height='579'></iframe></div>");
+}
---
+++ js/viewer.js
@@ -0,0 +1,26 @@
+$(function() {
+    var showFlug = false;
+    var topBtn = $('#viewer');    
+    topBtn.css('bottom', '-700px');
+    var showFlug = false;
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            if (showFlug == false) {
+                showFlug = true;
+                topBtn.stop().animate({'bottom' : '20px'}, 200); 
+            }
+        } else {
+            if (showFlug) {
+                showFlug = false;
+                topBtn.stop().animate({'bottom' : '-700px'}, 200); 
+            }
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
